1c122fbc2086dc062943cb3323b1b86c787cc220
aman <amannkhan1204@gmail.com>
2026-07-03 01:16:32 +0530

    Fix template filename case for Linux deployment

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -4,7 +4,7 @@ const interviewReportModel = require("../models/interviewReport.model")
 const {generatePdfFromHtml} = require("../utils/pdfGenerator");
 const {modernResumeTemplate} = require("../templates/modernResume.template");
 
-const atsResumeTemplate = require("../templates/atsResume.template");
+const atsResumeTemplate = require("../templates/AtsResume.template");
 
 /**
  * @description controller to generate report based on user self description, resume and job description. 
